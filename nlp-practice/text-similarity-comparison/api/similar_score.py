7a4779126cb7436f4e0f3f75bff7ac945cae7cf4
chenyuan <chenyuan@purvar.com>
2018-10-16 18:14:13 +0800

    Revert "修改接口"
    This reverts commit 950365a0190889e2fa6eb806a9fd84bd252b2fff.

--- nlp-practice/text-similarity-comparison/api/similar_score.py
+++ nlp-practice/text-similarity-comparison/api/similar_score.py
@@ -4,7 +4,7 @@ import os
 import json
 from input_handler import create_test_data, data_predict
 from config import siamese_config
-import base64
+
 
 class ConfiGuration(object):
     """Dump stuff here"""
@@ -21,24 +21,19 @@ w2index_file = os.path.join(config.data_dir, "model//w2index.txt")
 
 
 def show_similar_score(graph,nlp_model):
-    file_1 = request.form['filename1']
-    file_2 = request.form['filename2']
-    change_file_1 = base64.b64decode(file_1).decode('utf-8')
-    change_file_2 = base64.b64decode(file_2).decode('utf-8')
-    file_1_name = 'predict1.txt'
-    file_2_name = 'predict2.txt'
-    # if file_1_name.endswith('.txt') is False and file_2_name.endswith('.txt') is False:
-    #     return 'only support .txt'
+    file_1 = request.files['filename1']
+    file_2 = request.files['filename2']
+    file_1_name = file_1.filename
+    file_2_name = file_2.filename
+    if file_1_name.endswith('.txt') is False and file_2_name.endswith('.txt') is False:
+        return 'only support .txt'
     save_data_dir = os.path.join(upload_path,str(uuid.uuid1()))
+    print(save_data_dir)
     os.makedirs(save_data_dir)
     savefile_1 = os.path.join(save_data_dir,file_1_name)
-    with open(savefile_1,'w+',encoding='utf-8') as f:
-        f.write(change_file_1)
-    # file_1.save(savefile_1)
+    file_1.save(savefile_1)
     savefile_2 = os.path.join(save_data_dir,file_2_name)
-    # file_2 .save(savefile_2)
-    with open(savefile_2,'w+',encoding='utf-8') as f:
-        f.write(change_file_2)
+    file_2 .save(savefile_2)
     with open(w2index_file,'r+',encoding='utf-8') as f:
         w2index = f.read()
     index_dict = json.loads(w2index)
